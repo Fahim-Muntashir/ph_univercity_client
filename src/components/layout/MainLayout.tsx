15348174f7bf8fb50df5ec2112b74492631374f6
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-03-15 11:59:14 +0600

    a

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,70 @@
+import { Layout, Menu } from "antd";
+
+const { Header, Content, Footer, Sider } = Layout;
+import {
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { createElement } from "react";
+
+const MainLayout = () => {
+  const items = [
+    {
+      key: '1asfdasd',
+      label: "Dahboard"
+    },
+    {
+      key: '1asfdasda',
+      label: "Profile"
+    },
+    {
+      key: '1asfdsasd',
+      label: "Habib"
+    },
+
+  ]
+
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div style={{ color: "white", textAlign: "center", margin: "4px auto" }}>
+          <h1>PH Univercity</h1>
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["4"]}
+          items={items}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0 }} />
+        <Content style={{ margin: "24px 16px 0" }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            <h1>The Main Content</h1>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default MainLayout;
